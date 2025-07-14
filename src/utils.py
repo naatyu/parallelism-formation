edfc035e402ab9a8a30f90b41a2de469b9fb3a34
Natyu <94299955+Naatyu@users.noreply.github.com>
2025-07-14 16:33:17 +0200

    [CHORE] Removed name

--- src/utils.py
+++ src/utils.py
@@ -24,7 +24,7 @@ def get_logger(level: int = logging.INFO, log_file: Path | None = None) -> loggi
         console_handler = logging.StreamHandler()
         console_handler.setLevel(level)
         console_formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            "%(asctime)s - %(levelname)s - %(message)s",
         )
         console_handler.setFormatter(console_formatter)
         logger.addHandler(console_handler)
@@ -37,7 +37,7 @@ def get_logger(level: int = logging.INFO, log_file: Path | None = None) -> loggi
             file_handler = logging.FileHandler(str(log_file), mode="a")
             file_handler.setLevel(level)
             file_formatter = logging.Formatter(
-                "%(asctime)s - %(levelname)s - %(name)s - [%(filename)s:%(lineno)d] - %(message)s",
+                "%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s",
             )
             file_handler.setFormatter(file_formatter)
             logger.addHandler(file_handler)
