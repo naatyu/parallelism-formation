0ac3a32f41c7c98f69e69c34981e9889a8db7479
Natyu <94299955+Naatyu@users.noreply.github.com>
2025-07-15 22:59:42 +0200

    [FEAT] Added first exercise

--- src/1-data_loading/benchmark.py
+++ src/1-data_loading/benchmark.py
@@ -1,7 +1,101 @@
-"""Benchmark of different parameters of torch dataloader."""
+"""Benchmark of different implementations mades."""
 
-import torch
+import argparse
+import statistics
+import time
+from pathlib import Path
+
+import correction.ex1_cor as ex1
+from torch.utils.data import DataLoader
+
+from src.utils import logger
+
+
+def benchmark(loader):
+    step_times = []
+    for i in range(500):
+        step_start_time = time.time()
+        next(loader)
+        step_end_time = time.time()
+        elapsed_time = step_end_time - step_start_time
+
+        if i == 0:
+            step_first_time = elapsed_time
+        step_times.append(elapsed_time)
+    print(  # noqa: T201
+        "\n------------------------------------"
+        f"\nTime to first step: {step_first_time * 1e3:.2f} ms"
+        f"\nMean time/batch: {statistics.mean(step_times) * 1e3:.3f} ms"
+        f"\nMin time: {min(step_times) * 1e3:.3f} ms"
+        f"\nMax time: {max(step_times) * 1e3:.3f} ms"
+        "\n------------------------------------",
+    )
+
+
+def benchmark_gpu_transfert(loader):
+    step_times = []
+    for i in range(500):
+        batch = next(loader)
+        step_start_time = time.time()
+        batch.to("cuda")
+        step_end_time = time.time()
+        elapsed_time = step_end_time - step_start_time
+
+        if i == 0:
+            step_first_time = elapsed_time
+        step_times.append(elapsed_time)
+    print(  # noqa: T201
+        "\n------------------------------------"
+        f"\nTime to first step: {step_first_time * 1e3:.2f} ms"
+        f"\nMean time/batch: {statistics.mean(step_times) * 1e3:.3f} ms"
+        f"\nMin time: {min(step_times) * 1e3:.3f} ms"
+        f"\nMax time: {max(step_times) * 1e3:.3f} ms"
+        "\n------------------------------------",
+    )
+
 
 if __name__ == "__main__":
-    # Load dataset
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data-path",
+        type=Path,
+        default="data/sample/10BT",
+        dest="data_path",
+        help="Path to the data for tokenizer training.",
+    )
+
+    args = parser.parse_args()
+
+    # --- Bench simple map dataset --- #
+    simple_map_dataset = ex1.CustomTextDataset(args.data_path)
+    custom_loader = iter(
+        DataLoader(simple_map_dataset, batch_size=32, collate_fn=ex1.tokenize_collate),
+    )
+    logger.info("Simple map dataset")
+    benchmark(custom_loader)
+
+    # --- Bench multi-worker map dataset --- #
+    custom_multi_loader = iter(
+        DataLoader(
+            simple_map_dataset,
+            batch_size=32,
+            collate_fn=ex1.tokenize_collate,
+            num_workers=6,
+        ),
+    )
+    logger.info("Multi worker dataset")
+    benchmark(custom_multi_loader)
+
+    # --- Bench pin-memory dataset --- #
+    custom_pin_loader = iter(
+        DataLoader(
+            simple_map_dataset,
+            batch_size=32,
+            collate_fn=ex1.tokenize_collate,
+            pin_memory=True,
+        ),
+    )
+    logger.info("Without pin memory")
+    benchmark_gpu_transfert(custom_loader)
+    logger.info("With pin memory")
+    benchmark_gpu_transfert(custom_pin_loader)
---
+++ src/1-data_loading/ex1.py
@@ -0,0 +1,51 @@
+"""In this first exercise we will create a custom text dataset and it's dataloader.
+
+When implementing the dataset, be careful to the type hinting. Check also that the
+collate function return a valid batch (all squences should have the same shape).
+"""
+
+from pathlib import Path
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from torch.utils.data import DataLoader, Dataset
+
+from src.utils import logger
+
+tokenizer = Tokenizer.from_pretrained(
+    "mistralai/Mistral-7B-Instruct-v0.3",
+)  # TODO: update with local tokenizer later
+tokenizer.pad_token = "</s>"
+
+
+class CustomTextDataset(Dataset):
+    def __init__(self, data_dir: Path) -> None:
+        self.data = load_dataset("parquet", data_dir=data_dir, split="train")
+
+    def __len__(self) -> int:
+        raise NotImplementedError
+
+    def __getitem__(self, idx: int) -> str:
+        raise NotImplementedError
+
+
+def tokenize_collate(batch):
+    return tokenizer(
+        batch,
+        padding="max_length",
+        max_length=1024,
+        truncation=True,
+        return_tensors="pt",
+    )["input_ids"]
+
+
+if __name__ == "__main__":
+    map_dataset = CustomTextDataset("data/sample/10BT")
+    custom_loader = DataLoader(map_dataset, batch_size=4, collate_fn=tokenize_collate)
+
+    # Tests
+    assert len(map_dataset) == len(map_dataset.data)
+    assert type(map_dataset.__getitem__(0)) is str
+    assert map_dataset.__getitem__(0) == map_dataset.data[0]["text"]
+
+    logger.info("All tests passed !")
